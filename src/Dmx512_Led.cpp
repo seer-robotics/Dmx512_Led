0d355a2af1c0598371225cffc571b48ecec41c60
darboy <974782852@qq.com>
2018-07-05 10:09:31 +0800

    This is the codes and cmakelist in the src

---
+++ src/Dmx512_Led.cpp
@@ -0,0 +1,183 @@
+#include<Windows.h>
+#include<iostream>
+#include "Dmx512_Led.h"
+#include "message_odometer.pb.h"
+#include "message_battery.pb.h"
+#include <bitset>
+#include <strstream>
+#include <robokit/foundation/utils/geo_utils.h>
+#include <robokit/chasis/model.h>
+#include <robokit/core/error.h>
+#include <boost/lexical_cast.hpp>
+#include <robokit/chasis/model.h>
+#include <iostream>
+#include <errno.h>
+#include <signal.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+using namespace std;
+
+RBK_INHERIT_SOURCE(Dmx512_Led)
+
+Dmx512_Led::Dmx512_Led(){
+	_hx = new Clight();
+}
+
+Dmx512_Led::~Dmx512_Led(){
+	delete _hx;
+}
+void Dmx512_Led::loadFromConfigFile(){}
+//订阅里程、电池信息
+void Dmx512_Led::setSubscriberCallBack()
+{
+	setTopicCallBack<rbk::protocol::Message_Odometer>(&Dmx512_Led::messageDmx512_Led_Is_StopCallBack, this);
+	setTopicCallBack<rbk::protocol::Message_Battery>(&Dmx512_Led::messageDmx512_Led_BatteryCallBack, this);
+}
+//获取里程信息
+void Dmx512_Led::messageDmx512_Led_Is_StopCallBack(google::protobuf::Message* msg)
+{
+	m_Odometer.CopyFrom(*msg);
+	return;
+}
+//获取电池信息
+void Dmx512_Led::messageDmx512_Led_BatteryCallBack(google::protobuf::Message* msg)
+{
+	m_Battery.CopyFrom(*msg);
+	return;
+}
+
+void Dmx512_Led::run()
+{
+	while (true)
+	{
+		SLEEP(20);
+		Clight::EType et;
+		double param = 0;
+		if (rbk::ErrorCodes::Instance()->errorNum() || rbk::ErrorCodes::Instance()->fatalNum())
+		{
+			et = Clight::EFatalw;
+		}
+		else if(!m_Odometer.is_stop())
+		{
+			et = Clight::ERun;
+		}
+		else if(m_Odometer.is_stop())
+		{
+			param = m_Battery.percetage();
+			et = Clight::EStop;
+		}
+		_hx->update(et, param);
+		
+	}
+}
+Clight::Clight(){
+	_pILightDataCalcu[0] = new FatalWarrningCalcu();
+	_pILightDataCalcu[1] = new RunCalcu();
+	_pILightDataCalcu[2] = new BatteryCalcu();
+	_pCSerialport = new CSerialport();
+	_pCSerialport->init(L"COM4");
+
+
+}
+Clight::~Clight() {
+	if (NULL != _pILightDataCalcu)
+	{
+		delete _pILightDataCalcu;
+	}
+	if (NULL != _pCSerialport)
+	{
+		delete _pCSerialport;
+	}
+
+}
+
+
+void Clight::update(EType type, double param)
+{
+	_pILightDataCalcu[type]->calc(_data, param);
+	_pCSerialport->send(_data);
+	memset(_data, 0x00, sizeof(_data));
+}
+
+CSerialport::CSerialport(){
+	_hcom = NULL;
+}
+
+CSerialport::~CSerialport() {
+	CloseHandle(_hcom);
+}
+
+void CSerialport::init(CONST WCHAR *LPCWSTR)
+{
+	_hcom = CreateFile(LPCWSTR, GENERIC_WRITE, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (_hcom == INVALID_HANDLE_VALUE) 
+	{ 
+		cout << "DMX512 INIT FAILED!" << endl; 
+	} 
+	else
+	{
+		cout << "DMX512 CREATED SCUSSEC!" << endl;
+	}
+	SetupComm(_hcom, 1024, 1024);
+	DCB dcb;
+	GetCommState(_hcom, &dcb);
+	dcb.BaudRate = 250000;
+	dcb.ByteSize = 8;
+	dcb.Parity = NOPARITY;
+	dcb.StopBits = 2;
+	SetCommState(_hcom, &dcb);
+	
+}
+
+void CSerialport::send(const char *data)
+{
+	DWORD dwWrittenLen = 0;
+	SetCommBreak(_hcom);	Sleep(10);
+	ClearCommBreak(_hcom);	Sleep(0.1);
+	WriteFile(_hcom, data, 512, &dwWrittenLen, NULL);
+}
+
+void FatalWarrningCalcu::calc(char * data, double param)
+{
+	_aa = _aa + 4;
+	int bb = std::abs(_aa) * 2;
+	if (bb > 255)
+	{
+		bb = 255;
+	}
+	for (int i = 2; i <= 512; i = i + 4)
+	{
+		data[i] = bb; 
+	}
+}
+
+void RunCalcu::calc(char * data, double param)
+{
+	_aa = _aa + 4;
+	int bb = std::abs(_aa) * 2;
+	if (bb > 255)
+	{
+		bb = 255;
+	}
+	for (int i = 3; i <= 512; i = i + 4)
+	{
+		data[i] = bb;
+	}
+}
+
+void BatteryCalcu::calc(char * data, double param)
+{
+	int red = 0xFF * (1 - param);
+	int green = 0xFF * param;
+	for (int i = 1; i <= 512; i = i + 4)
+	{
+		data[i] = red;
+		data[i + 1] = green;
+	}
+}
+
+
+
+
---
+++ src/Dmx512_Led.h
@@ -0,0 +1,93 @@
+#ifndef _DMX512_LED_H_
+#define _DMX512_LED_H_
+#include <robokit/core/rbk_core.h>
+#include "message_odometer.pb.h"
+#include "message_battery.pb.h"
+#include <iostream>
+
+#define MAX_CALC_TYPE_NUM 10
+using namespace rbk;
+
+class Clight;
+class Dmx512_Led:public NPluginInterface
+{
+    public: 
+        Dmx512_Led();
+        ~Dmx512_Led();
+        void run();
+        void loadFromConfigFile();
+	    void setSubscriberCallBack();
+		void messageDmx512_Led_Is_StopCallBack(google::protobuf::Message* msg);
+		void messageDmx512_Led_BatteryCallBack(google::protobuf::Message* msg);
+    private:
+		rbk::protocol::Message_Odometer m_Odometer;
+		rbk::protocol::Message_Battery m_Battery;
+		rbk::MutableParam<uint32_t> _comNum;
+		Clight* _hx; 
+};
+
+class ILightDataCalcu;
+class CSerialport;
+class Clight
+{
+public:
+	enum EType { EFatalw = 0, ERun, EStop, EFlow };
+	Clight();
+	~Clight();
+	void update(EType type, double param);
+private:
+	ILightDataCalcu * _pILightDataCalcu[MAX_CALC_TYPE_NUM];
+	CSerialport * _pCSerialport;
+	char _data[512];
+
+};
+
+class CSerialport
+{
+	public:
+		CSerialport();
+		~CSerialport();
+		void init(CONST WCHAR *LPCWSTR);
+		void send(const char *pdata);
+	private:
+		HANDLE _hcom;
+};
+
+class ILightDataCalcu
+{
+	public:
+		virtual void calc(char * data, double param = 0) = 0;
+};
+
+class FatalWarrningCalcu : public ILightDataCalcu
+{
+	public:
+		FatalWarrningCalcu() { count = 1; brightness = 0x00; }
+		~FatalWarrningCalcu() {}
+		void calc(char * data, double param = 0);
+	private:
+		int count;
+		int brightness;
+		int8_t _aa = 0;
+};
+
+class RunCalcu : public ILightDataCalcu
+{
+	public:
+		RunCalcu(){ count = 1; brightness = 0x00; }
+		~RunCalcu(){}
+		void calc(char * data, double param = 0);
+	private:
+		int count;
+		int brightness;
+		int8_t _aa = 0;
+};
+
+class BatteryCalcu : public ILightDataCalcu
+{
+	public:
+		void calc(char * data, double param = 0);
+};
+
+RBK_INHERIT_PROVIDER(Dmx512_Led, NPluginInterface, "1.0.0");
+#endif
