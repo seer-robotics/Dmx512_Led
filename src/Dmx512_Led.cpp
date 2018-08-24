54028515c8ad543b7affb42d10a40999e03ec986
darboy <darboy@foxmail.com>
2018-08-24 11:47:48 +0800

    change the interface of calc & add the annotation

--- src/Dmx512_Led.cpp
+++ src/Dmx512_Led.cpp
@@ -1,11 +1,11 @@
 #include "Dmx512_Led.h"
-
 #include <robokit/chasis/model.h>
 #include <robokit/core/error.h>
 
 using namespace std;
 RBK_INHERIT_SOURCE(Dmx512_Led)
 
+/*The chanel of light always changes, define the chanel in correct position*/
 namespace {
 	const int CHANEL_RED = 1;
 	const int CHANEL_WHITE = 2;
@@ -22,6 +22,7 @@ Dmx512_Led::~Dmx512_Led() {
 	delete _hx;
 }
 
+/*By the roboshop ,can change the 7 params such as com, color_r, color_g, color_b, color_w, isShowCharging & isShowBattery*/
 void Dmx512_Led::loadFromConfigFile() {
 	loadParam(_com, "DmxCom", "COM4", rbk::ParamGroup::Chassis, "The com of Dmx512_Led");
 	LogInfo("The com of Dmx512 is " << _com  << " !");
@@ -34,20 +35,9 @@ void Dmx512_Led::loadFromConfigFile() {
 }
 
 void Dmx512_Led::setSubscriberCallBack() {
-	setTopicCallBack<rbk::protocol::Message_Odometer>(&Dmx512_Led::messageDmx512_Led_OdometerCallBack, this);
-	setTopicCallBack<rbk::protocol::Message_Battery>(&Dmx512_Led::messageDmx512_Led_BatteryCallBack, this);
-
+	setTopicCallBack<rbk::protocol::Message_Odometer>();
+	setTopicCallBack<rbk::protocol::Message_Battery>();
     rbk::chasis::Model::Instance()->connectChangedSignal(std::bind(&Dmx512_Led::modelChangedSubscriber, this));
-}
-
-void Dmx512_Led::messageDmx512_Led_OdometerCallBack(google::protobuf::Message* msg) {
-	m_Odometer.CopyFrom(*msg);
-	return;
-}
-
-void Dmx512_Led::messageDmx512_Led_BatteryCallBack(google::protobuf::Message* msg) {
-	m_Battery.CopyFrom(*msg);
-	return;
 }
 
 void Dmx512_Led::modelChangedSubscriber() {
@@ -60,57 +50,66 @@ void Dmx512_Led::modelChangedSubscriber() {
 
 void Dmx512_Led::run() {
     while (true) {
-
         while (true) {
             SLEEP(2000);
             if (_hx->_pCSerialport->init(_com.get())) {
                 break;
             }
         }
-
         while (true) {
             SLEEP(20);
-            getSubscriberData(m_Battery);
+            getSubscriberData(m_Battery); 
             getSubscriberData(m_Odometer);
             Clight::EType et = Clight::EConstantLight;
-            color_r = _color_r;
-            color_g = _color_g;
-            color_b = _color_b;
-            color_w = _color_w;
 
+			/*Defint a int arrary to add the color together,the param is set by user.
+			If use it to fix a certain color ,can redefine it before @setColor_RGBW*/
+			int RGBW[4] = { _color_r, _color_g, _color_b, _color_w };
             rbk::readLock l(_modelMutex);
             try {
-                /* if there exist erro or fatal ,it has the first priority*/
+				/*	ErrorFatal logic order:
+				1.Red breath light when regard the errornum > 0 or fatalnum > 0 as occur fatal or error
+				*/
                 if (rbk::ErrorCodes::Instance()->errorNum() > 0 || rbk::ErrorCodes::Instance()->fatalNum() > 0) {
                     et = Clight::EErrofatal;
                 }
 
-                /* run without erro or fatal*/
+				/*	Run logic order:
+				1.The chassis of encoder change or not
+				2.Regard the times of slight move >=10 as the chassis run  
+				3.the times not reach 10
+				*/
                 else if (!m_Odometer.is_stop()) {
                     if (is_stop_counts >= 10) {
                         et = Clight::EMutableBreath;
+						MutableBreathCalculator * pMutableBreath = dynamic_cast<MutableBreathCalculator * >(_hx->getPointOfICalculator(et)); 
+						pMutableBreath->setColor_RGBW(RGBW);
                     }
                     else {
-                        is_stop_counts++;    //for avoiding the misoperation
+                        is_stop_counts++;    //misoperation counter
                     }
                 }
 
-                /* battery logic*/
+                /*	Battery logic order:
+					1.Orange breath light when charging && param @isShowChargine is true
+					2.Red constant light when the percent of battery < 30% 
+					3.Red-Green constant light when the chassis is stop && param @isShowBattery is true
+					4.Constant light with color set by user 
+				*/ 
                 else if (_modelJson["chassis"]["batteryInfo"].get<uint32_t>() > 0) {
                     if (m_Battery.is_charging() && isShowCharging) {
                         et = Clight::ECharging;
                     }
                     else if (m_Battery.percetage() < 0.3) {
                         et = Clight::EConstantLight;
-                        color_r = 255;
-                        color_g = 0;
-                        color_b = 0;
-                        color_w = 0;
+						ConstantLightCalculator * pConstantLight = dynamic_cast<ConstantLightCalculator *>(_hx->getPointOfICalculator(et));
+						pConstantLight->setColor_RGBW(RGBW);
                     }
                     else if (m_Odometer.is_stop() && isShowBattery) {
                         is_stop_counts = 0;
-                        bttery_percetage = m_Battery.percetage();
                         et = Clight::EBattery;
+						BatteryCalculator * pbattery = dynamic_cast<BatteryCalculator *>(_hx->getPointOfICalculator(et));
+						pbattery->setBatteryPercent(m_Battery.percetage());
                     }
                     else {
                         et = Clight::EConstantLight;
@@ -120,8 +119,7 @@ void Dmx512_Led::run() {
             catch (const std::exception& e) {
                 LogError(e.what());
             }
-
-            if (!_hx->update(et, bttery_percetage, color_r, color_g, color_b, color_w)) {
+            if (!_hx->update(et)) {
                 break;
             }
         }
@@ -129,17 +127,17 @@ void Dmx512_Led::run() {
 }
 
 Clight::Clight() {
-	_pILightDataCalcu[0] = new ErroFatal();
-	_pILightDataCalcu[1] = new BatteryCalcu();
-	_pILightDataCalcu[2] = new MutableBreath();
-	_pILightDataCalcu[3] = new Charging();
-	_pILightDataCalcu[4] = new ConstantLight();
+	_pICalculator[0] = new ErroFatalCalculator();
+	_pICalculator[1] = new BatteryCalculator();
+	_pICalculator[2] = new MutableBreathCalculator();
+	_pICalculator[3] = new ChargingCalculator();
+	_pICalculator[4] = new ConstantLightCalculator();
 	_pCSerialport = new CSerialport();
 }
 
 Clight::~Clight() {
-	if (NULL != _pILightDataCalcu) {
-		delete[] _pILightDataCalcu;
+	if (NULL != _pICalculator) {
+		delete[] _pICalculator;
 	}
 	if (NULL != _pCSerialport) {
 		delete _pCSerialport;
@@ -147,9 +145,20 @@ Clight::~Clight() {
 	}
 }
 
-bool Clight::update(EType type, double param , int R, int G, int B, int W) {
+/**
+* @brief Calculate the data and send it
+* @param[in] type
+*		-EErrofatal
+*		-EBattery
+*		-EMutableBreath
+*		-ECharging
+*		-EConstantLight
+* @note Reset the data before calculate the data to ensure its data won't be disorder.
+* @retval -true Success -false Fail
+*/
+bool Clight::update(EType type) {
     memset(_data, 0x00, sizeof(_data));
-    _pILightDataCalcu[type]->calc(_data, param, R, G, B, W);
+    _pICalculator[type]->calc(_data);
     return _pCSerialport->send(_data, 512);
 }
 
@@ -160,6 +169,14 @@ CSerialport::~CSerialport() {
     _hcom.close();
 }
 
+/**
+* @brief Initialize the serial port
+* @param[in] com
+*		-"COM4"
+* @note Baud rate: 250000, stop bits:2, parity: none, character size:8
+* @return If successful loginfo,else logerro
+* @retval -true Success -false Fail
+*/
 bool CSerialport::init(const std::string& com) {
     boost::system::error_code ec;
     _hcom.open(com, 250000, boost::asio::serial_port_base::parity(
@@ -177,9 +194,16 @@ bool CSerialport::init(const std::string& com) {
 	}
 }
 
+/**
+* @brief Send data
+* @param data 
+* @param size -512
+* @note @SetCommBreak @ClearCommBreak
+* @retval -true Success -false Fail
+*/
 bool CSerialport::send(const char *data, size_t size) {
 	DWORD dwWrittenLen = 0;
-	SetCommBreak(_hcom.port().native_handle());   SLEEP(10);
+	SetCommBreak(_hcom.port().native_handle());   SLEEP(10);  //depand on its protocol
 	ClearCommBreak(_hcom.port().native_handle()); SLEEP(0.1);
     boost::system::error_code ec;
     _hcom.write(data, size, ec);
@@ -190,7 +214,13 @@ bool CSerialport::send(const char *data, size_t size) {
     return true;
 }
 
-void ErroFatal::calc(char * data, double param, int R, int G, int B, int W) {
+/**
+* @brief Error calculator
+* @param data
+* @note This is a breath light of red.
+* @return None
+*/
+void ErroFatalCalculator::calc(char * data) {
 	_increment = _increment + 4;
 	int final_value = std::abs(_increment) * 2;
 	if (final_value > 255)
@@ -203,9 +233,15 @@ void ErroFatal::calc(char * data, double param, int R, int G, int B, int W) {
 	}
 }
 
-void BatteryCalcu::calc(char * data, double param, int R, int G, int B, int W) {
-	int red = 0xFF * (1 - param);
-	int green = 0xFF * param;
+/**
+* @brief Battery calculator
+* @param data
+* @note "_param" depands on the percetage of battery .
+* @return None
+*/
+void BatteryCalculator::calc(char * data) {
+	int red = 0xFF * (1 - _param);
+	int green = 0xFF * _param;
 	for (int i = CHANEL_RED; i <= 50; i = i + 4)
 	{
 		data[i] = red;
@@ -213,7 +249,13 @@ void BatteryCalcu::calc(char * data, double param, int R, int G, int B, int W) {
 	}
 }
 
-void MutableBreath::calc(char * data, double param, int R, int G, int B, int W) {
+/**
+* @brief Mutable breath calculator
+* @param data
+* @note The value of data is constant scale change ,also a breath light but can change the color.
+* @return None
+*/
+void MutableBreathCalculator::calc(char * data) {
 	_increment = _increment + 4;
 	int final_value = std::abs(_increment) * 2;
 	if (final_value > 255)
@@ -222,24 +264,36 @@ void MutableBreath::calc(char * data, double param, int R, int G, int B, int W) 
 	}
 	for (int i = CHANEL_RED; i <= 50; i = i + 4)
 	{
-		data[i] = ((R * final_value) / 255);
-		data[i + 2] = ((G * final_value) / 255);
-		data[i + 3] = ((B * final_value) / 255);
-		data[i + 1] = ((W * final_value) / 255);
+		data[i] = ((_RGBW[0] * final_value) / 255);
+		data[i + 2] = ((_RGBW[1] * final_value) / 255);
+		data[i + 3] = ((_RGBW[2] * final_value) / 255);
+		data[i + 1] = ((_RGBW[3] * final_value) / 255);
 	}
 }
 
-void ConstantLight::calc(char * data, double param, int R, int G, int B, int W) {
+/**
+* @brief ConstantLight calculator
+* @param data
+* @note The value of data is a constant color.
+* @return None
+*/
+void ConstantLightCalculator::calc(char * data) {
 	for (int i = CHANEL_RED; i <= 50; i = i + 4)
 	{
-		data[i] = R;
-		data[i + 2] =G;
-		data[i + 3] = B;
-		data[i + 1] = W;
+		data[i] = _RGBW[0];
+		data[i + 2] = _RGBW[1];
+		data[i + 3] = _RGBW[2];
+		data[i + 1] = _RGBW[3];
 	}
 }
 
-void Charging::calc(char * data, double param, int R, int G, int B, int W) {
+/**
+* @brief Charging calculator
+* @param data
+* @note Constant breath light of orange color.
+* @return None
+*/
+void ChargingCalculator::calc(char * data) {
 	/* Orange color is R:255 and B:165 */
 	_increment = _increment + 4;
 	int final_value = std::abs(_increment) * 2;
